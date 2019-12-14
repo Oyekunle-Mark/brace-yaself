8c7fdfcf823c962fb87b7cfb27d4298568b94a67
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-14 17:11:33 +0100

    The function lat?

--- lat.scm
+++ lat.scm
@@ -1,3 +1,10 @@
 ; lat? looks at each S-expression in a list, in
 ; turn, and asks if each S-expression is an atom, 
 ; until it runs out of S-expressions.
+
+(define lat?
+    (lambda (l)
+        (cond
+            ((null? l) #t)
+            ((atom? (car l)) (lat? (cdr l)))
+            (else #f))))
