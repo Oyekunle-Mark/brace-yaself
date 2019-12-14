ecd0650044684e4a17527f5839684098529516a5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-14 17:18:20 +0100

    The function member

--- member.scm
+++ member.scm
@@ -1,3 +1,9 @@
 ; member takes two arguments
 ; checks if the S-expression which is the first argument
 ; is present in the list which is the second argument
+
+(define member?
+    (lambda (a l)
+        (cond 
+            ((null? l) #f)
+            (else or (eq? (car l) a) (member? a (cdr l))))))
