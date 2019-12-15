ca70819cc58f20f4481ca7c016cbe8e712dd4088
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 21:56:43 +0100

    Implementaion of insertR

--- insertR.scm
+++ insertR.scm
@@ -1,3 +1,12 @@
 ; It takes three arguments: the atoms new and old, and a lat. 
 ; The function insertR builds a lat 
 ; with new inserted to the right of the first occurrence of old
+
+(define insertR
+    (lambda (new old lat)
+        (cond 
+            ((null? lat) (quote ()))
+            ((eq? (car lat) old) (cons old
+                (cons new (cdr lat))))
+            (else (cons (car lat)
+                (insertR new old (cdr lat)))))))
