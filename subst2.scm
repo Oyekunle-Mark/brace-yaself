89462b868e1f240169e3531105e372f7fac91660
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 22:19:07 +0100

    Subst2

--- subst2.scm
+++ subst2.scm
@@ -1,4 +1,12 @@
 ; subst replaces either the first occurrence of o1
 ; or the first occurrence of o2 by new
 
-
+(define subst2
+    (lambda (new o1 o2 lat)
+        (cond 
+            ((null? lat) (quote ()))
+            (else (cond 
+                ((eq? (car lat) o1) (cons new (cdr lat)))
+                ((eq? (car lat) o2) (cons new (cdr lat)))
+                (else
+                    (cons (car lat) (subst new old (cdr lat)))))))))
