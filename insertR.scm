c7b69d44ae6acf29223e793ab8dc8a50ef55005a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 21:59:17 +0100

    Improved the use of cond

--- insertR.scm
+++ insertR.scm
@@ -6,7 +6,8 @@
     (lambda (new old lat)
         (cond 
             ((null? lat) (quote ()))
-            ((eq? (car lat) old) (cons old
-                (cons new (cdr lat))))
-            (else (cons (car lat)
-                (insertR new old (cdr lat)))))))
+            (else (cond
+                ((eq? (car lat) old) (cons old
+                    (cons new (cdr lat))))
+                (else (cons (car lat)
+                    (insertR new old (cdr lat)))))))))
