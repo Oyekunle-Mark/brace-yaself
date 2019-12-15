f559514345fd9bcbb6ce40d59c39b8c7ee22b16b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 21:42:39 +0100

    Firsts implementation

--- firsts.scm
+++ firsts.scm
@@ -1,3 +1,10 @@
-; firsts takes a list as argument lat
+; firsts takes a list as argument l
 ; returns a new list with the first S-expression
-; in the list in lat
+; in the list in l
+
+(define firsts
+    (lambda (l)
+        (cond 
+            ((null? l) (quote ()))
+            (else (cons (car (car l))
+                (firsts (cdr l)))))))
