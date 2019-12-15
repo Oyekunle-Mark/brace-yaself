03935ee07c41c2b36ffe4ffecafb950f6d836073
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 21:26:15 +0100

    Implementation of rember

--- rember.scm
+++ rember.scm
@@ -2,3 +2,11 @@
 ; it takes two arguments
 ; returns a new list which does not have the first 
 ; occurence of the first S-expression
+
+(define rember
+    (lambda (a lat)
+        (cond 
+            ((null? lat) (quote ()))
+            ((eq? (car lat) a) (cdr lat))
+            (else (cons (car lat)
+                (rember a (cdr lat)))))))
