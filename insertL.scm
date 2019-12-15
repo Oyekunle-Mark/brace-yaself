e7bbeb685cdccc69977a7bbcd25b561190650672
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 22:07:51 +0100

    The program insertL

--- insertL.scm
+++ insertL.scm
@@ -1,3 +1,12 @@
 ; It takes three arguments: the atoms new and old, and a lat. 
 ; The function insertR builds a lat 
 ; with new inserted to the lef of the first occurrence of old in lat
+
+(define insertL
+    (lambda (new old lat)
+        (cond 
+            ((null? lat) (quote ()))
+            (else (cond
+                ((eq? (car lat) old) (cons new lat))
+            (else (cons (car lat)
+                (insertL new old (cdr lat))))))))
