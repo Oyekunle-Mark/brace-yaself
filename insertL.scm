d9656e25c1341d299b28eac52de19bfcf236f38c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 22:00:22 +0100

    Better definition

--- insertL.scm
+++ insertL.scm
@@ -1,3 +1,3 @@
 ; It takes three arguments: the atoms new and old, and a lat. 
 ; The function insertR builds a lat 
-; with new inserted to the lef of the first occurrence of old
+; with new inserted to the lef of the first occurrence of old in lat
