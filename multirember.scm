6d5277198770c15be611590abaa5b134cd119812
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 22:26:58 +0100

    Recursive rember

--- multirember.scm
+++ multirember.scm
@@ -1,2 +1,11 @@
 ; multirember gives as its final value the lat
 ; with all occurrences of a removed
+
+(define multirember
+    (lambda (a lat)
+        (cond 
+            ((null? lat) (quote ()))
+            (else (cond 
+                ((eq? (car lat) a) (multirember a (cdr lat)))
+                (else
+                    (cons (car lat) (multirember a (cdr lat)))))))))
