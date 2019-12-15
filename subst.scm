ecf1acc5f4e50a415b230f6998ff524a137d643c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-15 22:17:02 +0100

    The function subst

--- subst.scm
+++ subst.scm
@@ -1,1 +1,10 @@
 ; subst replaces the first occurrence of old in the lat with new
+
+(define subst
+    (lambda (new old lat)
+        (cond 
+            ((null? lat) (quote ()))
+            (else (cond 
+                ((eq? (car lat) old) (cons new (cdr lat)))
+                (else
+                    (cons (car lat) (subst new old (cdr lat)))))))))
